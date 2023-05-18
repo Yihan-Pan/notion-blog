2eecbcd57df127a54b026d041f59ed1c20c33945
Yihan-Pan <133894032+Yihan-Pan@users.noreply.github.com>
2023-05-18 11:14:52 +0100

    Update site.config.ts: description

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Yihan Pan',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Yihan's Personal Site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
